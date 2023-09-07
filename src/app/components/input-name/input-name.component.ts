d715421397f76ad1a48940d8e6168b6f35907f90
tumsifu lema <tom455976@gmail.com>
2023-09-07 08:00:13 +0300

    added validation to name form

--- src/app/components/input-name/input-name.component.ts
+++ src/app/components/input-name/input-name.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputNameComponent  implements OnInit {
 
+public firstName!:string;
+public secondName!:string;
+
+/*  public fullName =[
+    {
+      ariaLabel:true,
+      placeHolder:"First Name",
+      modelOf:"firstName",
+      idOf:"firstName",
+      minOf:"2",
+      nameOf:"firstName",
+    },
+    {
+      ariaLabel:true,
+      placeHolder:"Last Name",
+      modelOf:"lastName",
+      idOf:"lastName",
+      minOf:"4",
+      nameOf:"lastName",
+    }
+  ]  
+;  */
   constructor() { }
 
   ngOnInit() {}
